474d14f7ee41ec917c47020b4644354a05e25a26
Julian Betts <wjulianbetts@gmail.com>
2023-02-03 14:24:40 -0800

    added about me

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Aboutme from './components/Aboutme' //react
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Julian</h1>
+      <Aboutme/>
+    </div>
+  );
+}
+
+export default App;
