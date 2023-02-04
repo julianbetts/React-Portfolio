9006f5ff0dd6727d849197928351cde95446e2ae
Julian Betts <wjulianbetts@gmail.com>
2023-02-03 18:54:32 -0800

    adjustments

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,18 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>Portfolio</h1>
       <p>
-        Julian Betts is a coder based in Los Angeles.
+      <ul class="list-group">
+              
+              
+                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/tastytunage/" class="btn btn-primary">Tasty Tunage</a></li>
+                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/passwordgenerator/" class="btn btn-primary">Password Generator</a></li>
+                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/weather/" class="btn btn-primary">Weather App</a></li>
+                <li class="list-group-item list-group-item-primary"> <a href="https://mountain-express.herokuapp.com/" class="btn btn-primary">Mountain Express</a></li>
+
+              </ul>
+      
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,18 +3,9 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Julian Betts Portfolio</h1>
+      <h1>Julian Betts</h1>
       <p>
-      <ul class="list-group">
-                <li class="list-group-item list-group-item-secondary">Repos</li>
-              
-              
-                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/tastytunage/" class="btn btn-primary">Tasty Tunage</a></li>
-                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/passwordgenerator/" class="btn btn-primary">Password Generator</a></li>
-                <li class="list-group-item list-group-item-primary"> <a href="https://julianbetts.github.io/weather/" class="btn btn-primary">Weather App</a></li>
-                <li class="list-group-item list-group-item-primary"> <a href="https://mountain-express.herokuapp.com/" class="btn btn-primary">Mountain Express</a></li>
-
-              </ul>
+      <div>Julian Betts is a full stack developer based in Los Angeles, California.</div>
       </p>
     </div>
   );
